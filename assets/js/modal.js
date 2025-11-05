7d4846c1e2a1425bb9a6ba43d43b34955070b71b
iPAGroove <ipagroove@gmail.com>
2025-11-05 19:54:35 +0200

    Update modal.js

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -47,7 +47,6 @@ export function openModal(data) {
     document.getElementById('modal-time-ago').textContent = timeAgo;
     
     // Функции мода: Заменяем запятые на переносы строк И УДАЛЯЕМ ЗАПЯТЫЕ
-    // Заменяем запятую и пробел на перенос строки, затем удаляем все оставшиеся запятые
     document.getElementById('modal-features').textContent = data.features
         .replace(/, /g, '\n') // Заменяем ", " на новую строку
         .replace(/,/g, '\n'); // Заменяем оставшиеся одиночные запятые на новую строку
