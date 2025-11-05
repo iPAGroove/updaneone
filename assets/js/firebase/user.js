80d7cf7a40ef56369e64f9626c318b507534b0c5
iPAGroove <ipagroove@gmail.com>
2025-11-05 23:11:02 +0200

    Update user.js

--- assets/js/firebase/user.js
+++ assets/js/firebase/user.js
@@ -1,16 +1,11 @@
 // assets/js/firebase/user.js
 
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { auth } from "../app.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
-const auth = getAuth();
-
-// callback → вызывается при изменении пользователя
 export function onUserChanged(callback) {
     onAuthStateChanged(auth, (user) => {
-        if (!user) {
-            callback(null);
-            return;
-        }
+        if (!user) return callback(null);
 
         callback({
             uid: user.uid,
