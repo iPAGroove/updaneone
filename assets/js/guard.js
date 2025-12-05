d2c6eb753a27e79b2250c601e53e7346ccd927e1
iPAGroove <ipagroove@gmail.com>
2025-12-05 17:09:22 +0200

    Update guard.js

--- assets/js/guard.js
+++ assets/js/guard.js
@@ -3,11 +3,11 @@ export function checkAccess() {
                 window.navigator.standalone;
 
   if (!isPwa) {
-    console.log("⛔ home.html открыт НЕ как PWA — назад на index");
+    console.log("⛔ Открыт не как PWA — возврат на index");
     window.location.href = "index.html";
     return false;
   }
 
-  console.log("✔️ PWA режим подтверждён, можно грузить приложение");
+  console.log("✔️ PWA доступ подтверждён");
   return true;
 }
