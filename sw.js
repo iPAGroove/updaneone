c8a2b2e1c0146adf0e8cf81b30d3630a40469f32
iPAGroove <ipagroove@gmail.com>
2025-12-05 17:07:30 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", () => {
+  console.log("📦 Service Worker: installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  console.log("⚡ Service Worker: activated");
+});
