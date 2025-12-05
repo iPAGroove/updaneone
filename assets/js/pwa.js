903a5c9dc369af03732a2a2bc6c7bd7b0fad01a5
iPAGroove <ipagroove@gmail.com>
2025-12-05 16:37:12 +0200

    Update pwa.js

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -4,14 +4,3 @@ if ("serviceWorker" in navigator) {
         .then(() => console.log("🛠️ SW зарегистрирован"))
         .catch(err => console.warn("SW error:", err));
 }
-
-// Проверяем, что запущено как PWA
-function isPwa() {
-    return window.matchMedia("(display-mode: standalone)").matches ||
-           window.navigator.standalone;
-}
-
-if (!isPwa()) {
-    console.log("⚠️ Не PWA → на инструкцию");
-    window.location.href = "/index.html";
-}
