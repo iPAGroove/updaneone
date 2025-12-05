5e89742aef61d8ea6ac5b55599847bbd68667827
iPAGroove <ipagroove@gmail.com>
2025-12-05 17:09:54 +0200

    Delete pwa/sw.js

--- pwa/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("📦 Service Worker: installed");
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  console.log("⚡ Service Worker: activated");
-});
