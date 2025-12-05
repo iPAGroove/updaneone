8a094e027361af0807e3c67848071c994f512176
iPAGroove <ipagroove@gmail.com>
2025-12-05 15:14:02 +0200

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,3 +1,4 @@
+const CACHE_VERSION = "v2"; // поменяли версию!
+
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
-self.addEventListener("fetch", () => {});
