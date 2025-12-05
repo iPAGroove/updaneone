2c11bbb6553818b19737db1c2c80b46577e68f27
iPAGroove <ipagroove@gmail.com>
2025-12-05 17:00:56 +0200

    Update pwa.js

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -1,6 +1,6 @@
 // Регистрируем Service Worker (дублирует регистрацию, но не страшно)
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/pwa/sw.js")
+  navigator.serviceWorker.register("pwa/sw.js")
     .then(() => console.log("🛠️ SW зарегистрирован с home.html"))
     .catch(err => console.warn("SW error:", err));
 }
