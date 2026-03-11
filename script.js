36057a4b8c35fc7db25f906648771f7a9f9f58a8
iPAGroove <ipagroove@gmail.com>
2026-03-11 23:08:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Ищем параметр udid в адресной строке
     const urlParams = new URLSearchParams(window.location.search);
     const udid = urlParams.get('udid');
 
     if (udid) {
-        // Если UDID есть, показываем его
         document.getElementById('udid-display').textContent = udid;
         document.getElementById('result').classList.remove('hidden');
     }
