046c7e71e9ad2a726138e97aa15525939b84c42c
iPAGroove <ipagroove@gmail.com>
2026-03-11 22:42:15 +0200

    Delete assets directory

--- assets/js/pwa.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js")
-    .then(() => console.log("🛠️ SW зарегистрирован (home.html)"))
-    .catch(err => console.warn("SW error:", err));
-}
